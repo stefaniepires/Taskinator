4788de280869f60b1a43bf316c1f02bddc3ea27e
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-02 22:27:38 -0600

    Edit Task Button has Functionality now

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,15 +20,37 @@ var taskFormHandler = function(event) {
     // This erases the previous values that the user input to make the form more user friendly 
     formEl.reset();
  
-// package up data as an object
-var taskDataObj = {
-    name: taskNameInput,
-    type: taskTypeInput
-  };
-
-  // send it as an argument to createTaskEl
-  createTaskEl(taskDataObj);
+var isEdit = formEl.hasAttribute("data-task-id");
+// has data attribute, so get task id and call function to complete edit process
+if (isEdit) {
+    var taskId = formEl.getAttribute("data-task-id");
+    completeEditTask(taskNameInput, taskTypeInput, taskId);
+  } 
+  // no data attribute, so create object as normal and pass to createTaskEl function
+  else {
+    var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+    };
+  
+    createTaskEl(taskDataObj);
   }
+}
+
+var completeEditTask = function(taskName, taskType, taskId) {
+    // find the matching task list item
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+// set new values
+taskSelected.querySelector("h3.task-name").textContent = taskName;
+taskSelected.querySelector("span.task-type").textContent = taskType;
+
+alert("Task Updated!");
+
+formEl.removeAttribute("data-task-id");
+document.querySelector("#save-task").textContent = "Add Task";
+};
+
 
 var createTaskEl = function(taskDataObj) {
 
