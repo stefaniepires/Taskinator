7d414c872d5b4331bc5b52c877e8d109df493365
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-02 22:34:54 -0600

    add ability to update and delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var tasksInProgressEl = document.querySelector("#tasks-in-progress");
+var tasksCompletedEl = document.querySelector("#tasks-completed");
 
 var taskFormHandler = function(event) {
 
@@ -156,5 +158,23 @@ var editTask = function(taskId) {
     formEl.setAttribute("data-task-id", taskId);
 };
 
+var taskStatusChangeHandler = function(event) {
+//get task items ID 
+var taskId = event.target.getAttribute("data-task-id");
+var statusValue = event.target.value.toLowerCase();
+var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+if (statusValue === "to do") {
+    tasksToDoEl.appendChild(taskSelected);
+}
+else if (statusValue === "in progress") {
+    tasksInProgressEl.appendChild(taskSelected);
+}
+else if (statusValue === "completed") {
+    tasksCompletedEl.appendChild(taskSelected);
+}
+
+};
 
 pageContentEl.addEventListener("click", taskButtonHandler);
+
+pageContentEl.addEventListener("change", taskStatusChangeHandler);
