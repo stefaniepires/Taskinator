e20fb4fb2a2a98646f6b5899195e169f9732cf87
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-04 16:44:39 -0600

    added styling to drag drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -198,6 +198,7 @@ var dropZoneDragHandler = function (event) {
   var taskListEl = event.target.closest(".task-list");
   if (taskListEl) {
     event.preventDefault();
+  taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
   }
 };
 
@@ -220,7 +221,16 @@ var dropTaskHandler = function (event) {
      statusSelectEl.selectedIndex=2;
  }
 dropZoneEl.appendChild(draggableElement);
+
+dropZoneEl.removeAttribute("style");
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+      taskListEl.removeAttribute("style");
+    }
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
@@ -231,3 +241,5 @@ pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 
 pageContentEl.addEventListener("drop", dropTaskHandler);
+
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
