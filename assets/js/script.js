91c6a961159968b5c26476837fb1c2424744247e
Stef Tinsley <stefaniectinsley@gmail.com>
2020-11-04 18:01:18 -0600

    added save tasks function and  JSON stringify

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,6 +63,8 @@ var completeEditTask = function (taskName, taskType, taskId) {
 
   formEl.removeAttribute("data-task-id");
   document.querySelector("#save-task").textContent = "Add Task";
+
+  saveTasks();
 };
 
 var createTaskEl = function (taskDataObj) {
@@ -103,8 +105,7 @@ var createTaskEl = function (taskDataObj) {
   //increase task counter for next unique id
   taskIdCounter++;
 
-  console.log(taskDataObj);
-  console.log(taskDataObj.status);
+  saveTasks();
 };
 
 var createTaskActions = function (taskId) {
@@ -173,19 +174,21 @@ var deleteTask = function (taskId) {
   );
   taskSelected.remove();
 
-    // create new array to hold updated list of tasks
-var updatedTaskArr = [];
+  // create new array to hold updated list of tasks
+  var updatedTaskArr = [];
 
-// loop through current tasks
-for (var i = 0; i < tasks.length; i++) {
-  // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
-  if (tasks[i].id !== parseInt(taskId)) {
-    updatedTaskArr.push(tasks[i]);
+  // loop through current tasks
+  for (var i = 0; i < tasks.length; i++) {
+    // if tasks[i].id doesn't match the value of taskId, let's keep that task and push it into the new array
+    if (tasks[i].id !== parseInt(taskId)) {
+      updatedTaskArr.push(tasks[i]);
+    }
   }
-}
 
-// reassign tasks array to be the same as updatedTaskArr
-tasks = updatedTaskArr;
+  // reassign tasks array to be the same as updatedTaskArr
+  tasks = updatedTaskArr;
+
+  saveTasks();
 };
 
 var editTask = function (taskId) {
@@ -221,7 +224,8 @@ var taskStatusChangeHandler = function (event) {
       tasks[i].status = statusValue;
     }
   }
-  console.log(tasks);
+
+  saveTasks();
 };
 
 //for dragstart
@@ -265,15 +269,13 @@ var dropTaskHandler = function (event) {
 
   dropZoneEl.removeAttribute("style");
 
-
   //loop through tasks array to find and update the updated task's status
   for (var i = 0; i < tasks.length; i++) {
     if (tasks[i].id === parseInt(id)) {
       tasks[i].status = statusSelectEl.value.toLowerCase();
     }
-
-    console.log(tasks);
-  };
+  }
+  saveTasks();
 };
 
 var dragLeaveHandler = function (event) {
@@ -281,9 +283,11 @@ var dragLeaveHandler = function (event) {
   if (taskListEl) {
     taskListEl.removeAttribute("style");
   }
-}
+};
 
-
+var saveTasks = function () {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+};
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 
